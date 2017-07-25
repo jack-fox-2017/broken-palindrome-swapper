fc474a81ec67b4eeb80633e9cc61a43a9142d857
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-07-25 20:51:30 +0700

    palindrome selesai

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -1,4 +1,4 @@
-/**
+/*
  * @function palindromeSwapper
  * menukar 2 huruf berurutan dari n = 0 sampai n-1
  * contoh: makan => amkan, mkaan, maakn, makna
@@ -7,32 +7,40 @@
  */
 function palindromeSwapper(str) {
   let newStr = '';
+  // if (isPalindrome(str))
+  //   return true;
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
         newStr += str[j+1];
         newStr += str[j];
+        j++;
       } else {
         newStr += str[j];
       }
-      j++;
     }
-    if (isPalindrome(newStr)) return true;
+    if (isPalindrome(newStr)) {
+      return true;
+    }
   }
   return false;
 }
 
-/**
+/*
  * @function isPalindrome
  * Cek apabila kata merupakan palindromeSwapper
  * @param {string} str - input kata yang dicek bila palindrom
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true
+  return false
 }
 
+console.log(palindromeSwapper('maham'));
+console.log(palindromeSwapper('amham'));
+console.log(palindromeSwapper('kodok'));
+console.log(palindromeSwapper('okdok'));
 console.log(palindromeSwapper('arcecar')); // TRUE
 console.log(palindromeSwapper('racecar')); // TRUE
 console.log(palindromeSwapper('recacar')); // FALSE
